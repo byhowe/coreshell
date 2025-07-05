03b9667b3975dfcaae729f019b6f11ae6c43f52a
Ömer Faruk Çavuş <37745048+byhowe@users.noreply.github.com>
2025-07-05 21:13:07 +0300

    Draw a black status bar.

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,23 @@
+use std::os::fd::AsFd;
+use std::ptr;
+use std::slice;
+
+use rustix::fs::MemfdFlags;
+use rustix::fs::ftruncate;
+use rustix::fs::memfd_create;
+use rustix::mm;
 use wayland_client::Connection;
 use wayland_client::Dispatch;
 use wayland_client::QueueHandle;
 use wayland_client::delegate_noop;
+use wayland_client::protocol::wl_buffer;
+use wayland_client::protocol::wl_buffer::WlBuffer;
 use wayland_client::protocol::wl_compositor::WlCompositor;
 use wayland_client::protocol::wl_registry;
 use wayland_client::protocol::wl_registry::WlRegistry;
+use wayland_client::protocol::wl_shm;
 use wayland_client::protocol::wl_shm::WlShm;
+use wayland_client::protocol::wl_shm_pool::WlShmPool;
 use wayland_client::protocol::wl_surface::WlSurface;
 use wayland_protocols_wlr::layer_shell::v1::client::zwlr_layer_shell_v1::ZwlrLayerShellV1;
 use wayland_protocols_wlr::layer_shell::v1::client::zwlr_layer_surface_v1;
@@ -107,7 +119,7 @@ impl Dispatch<WlRegistry, ()> for State
                 // Anchor top, left, right. This makes the bar appear on the top of the screen
                 // stretching from left to right.
                 layer_surface.set_anchor(Anchor::Top | Anchor::Left | Anchor::Right);
-                layer_surface.set_exclusive_zone(HEIGHT as _);
+                layer_surface.set_exclusive_zone(HEIGHT as i32);
                 surface.commit();
                 state.layer_surface = Some(layer_surface);
             }
@@ -118,22 +130,108 @@ impl Dispatch<WlRegistry, ()> for State
 impl Dispatch<ZwlrLayerSurfaceV1, ()> for State
 {
     fn event(
-        _state: &mut Self,
-        _layer_surface: &ZwlrLayerSurfaceV1,
+        state: &mut Self,
+        layer_surface: &ZwlrLayerSurfaceV1,
         event: zwlr_layer_surface_v1::Event,
+        _data: &(),
+        _conn: &Connection,
+        qh: &QueueHandle<Self>,
+    )
+    {
+        // Receive information from the compositor about the width and height of
+        // the bar. We need this information to create an appropriately sized
+        // buffer in ram to render our pixels to.
+
+        if let zwlr_layer_surface_v1::Event::Configure {
+            serial,
+            width,
+            height,
+        } = event
+        {
+            println!("Configure Event received");
+
+            layer_surface.ack_configure(serial);
+
+            let pixels = width * height;
+            // Multiply by 4, because each pixel occupies 4 bytes.
+            let size = pixels * 4;
+            let stride = width * 4;
+
+            // Create a buffer in memory.
+            let memfd = memfd_create("coreshell", MemfdFlags::empty()).unwrap();
+            // Its size is set by ftruncate.
+            ftruncate(&memfd, size as u64).unwrap();
+            // Map the file descriptor into memory.
+            let ptr = unsafe {
+                mm::mmap(
+                    ptr::null_mut(),
+                    size as usize,
+                    mm::ProtFlags::READ | mm::ProtFlags::WRITE,
+                    mm::MapFlags::SHARED,
+                    &memfd,
+                    0,
+                )
+            }
+            .unwrap();
+            // Treat the raw pointer obtained by mmap as a u32 slice.
+            let data = unsafe { slice::from_raw_parts_mut(ptr.cast::<u32>(), pixels as usize) };
+
+            // Fill opaque black.
+            data.iter_mut().for_each(|pixel| *pixel = 0xFF000000);
+
+            // Create shm pool and buffer.
+            // NOTE: When memfd goes out of scope at the end of this function, its drop
+            // function will get called and the file descriptor associated with
+            // it will be closed. It may happen that the file descriptor closes
+            // before it is received by the compositor. In which case, the
+            // compositor will fail to open the descriptor (if I understand
+            // correctly). However, it seems that file descriptors are sent differently as
+            // seen in https://docs.rs/wayland-backend/0.3.10/src/wayland_backend/rs/socket.rs.html#42.
+            // The file descriptor received by the compositor will be different than the one
+            // we sent. So, I think it is safe for memfd to drop at the end of
+            // this scope.
+            let shm = state.shm.as_ref().unwrap();
+            let shm_pool = shm.create_pool(memfd.as_fd(), size as i32, qh, ());
+            let buffer = shm_pool.create_buffer(
+                0,
+                width as i32,
+                height as i32,
+                stride as i32,
+                wl_shm::Format::Argb8888,
+                qh,
+                (),
+            );
+            // NOTE: It is safe to destory the pool after creating a buffer from it.
+            // https://wayland.app/protocols/wayland#wl_shm_pool:request:create_buffer
+            shm_pool.destroy();
+
+            let surface = state.surface.as_ref().unwrap();
+            surface.attach(Some(&buffer), 0, 0);
+            surface.commit();
+        }
+    }
+}
+
+impl Dispatch<WlBuffer, ()> for State
+{
+    fn event(
+        _state: &mut Self,
+        buffer: &WlBuffer,
+        event: wl_buffer::Event,
         _data: &(),
         _conn: &Connection,
         _qh: &QueueHandle<Self>,
     )
     {
-        // Receive information from the compositor about the width and height of the
-        // bar. We need this information to create an appropriately sized buffer
-        // in ram to render our pixels to.
-        println!("{:?}", event);
+        if let wl_buffer::Event::Release = event {
+            println!("The compositor has released the buffer");
+            buffer.destroy();
+        }
     }
 }
 
 delegate_noop!(State: ignore WlCompositor);
 delegate_noop!(State: ignore WlSurface);
 delegate_noop!(State: ignore WlShm);
+delegate_noop!(State: ignore WlShmPool);
 delegate_noop!(State: ignore ZwlrLayerShellV1);
